174c59de2ceccd49aefe369cca0a99330bc3333f
Lukas Kesselburg <lukas.kesselburg@gmail.com>
2020-05-08 08:25:51 +0200

    Vorerst nur der Code. Kommentare folgen.

---
+++ src/Hausaufgaben/HA06/TreeNode.java
@@ -0,0 +1,17 @@
+package Hausaufgaben.HA06;
+
+public class TreeNode {
+	public int value;
+	public TreeNode left;
+	public TreeNode right;
+
+	
+	public TreeNode(int value) {
+		this.value=value;
+	}
+	
+	
+	
+	
+
+}
